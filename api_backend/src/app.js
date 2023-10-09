6c124b56e5c244afb70500961b98fe7edc8d29af
Wblanco0315 <wowblanco071@gmail.com>
2023-10-09 13:40:06 -0500

    Api frontend

--- api_backend/src/app.js
+++ api_backend/src/app.js
@@ -10,6 +10,15 @@ import usersRoutes from "./routes/user.routes.js";
 const app = express();
 createRoles();
 
+const corsOptions = {
+    origin: 'https://mi-front-end.com',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+    optionsSuccessStatus: 204,
+    credentials: true,
+  };
+  
+
 app.use(morgan('dev'));
 
 app.use(express.json());
@@ -17,5 +26,5 @@ app.use(express.json());
 app.use('/api/products', productsRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/users', usersRoutes);
-
+app.use(cors(corsOptions));
 export default app;
