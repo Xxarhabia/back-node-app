3a6359416a69e1c6bc8593bac6371824b28316b6
Xxarhabia <msarabiacarrascal@gmail.com>
2023-11-21 21:47:03 -0500

    se agregan las rutas para usuario

--- api_backend/src/routes/user.routes.js
+++ api_backend/src/routes/user.routes.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
-import { createUser } from "../controllers/user.controller.js";
+import {
+  createUser,
+  getUserById,
+  getUsers,
+  updateUserAddress,
+} from "../controllers/user.controller.js";
 import { veriryToken, isAdmin } from "../middlewares/authJwt.js";
 import {
   checkRolesExisted,
@@ -13,5 +18,8 @@ router.post(
   [veriryToken, isAdmin, checkRolesExisted, checkDuplicateUsernameOrEmail],
   createUser
 );
+router.get("/", [veriryToken, isAdmin], getUsers);
+router.get("/:id", [veriryToken, isAdmin], getUserById);
+router.put("/:id", [veriryToken, isAdmin], updateUserAddress);
 
 export default router;
